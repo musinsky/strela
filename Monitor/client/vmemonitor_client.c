9719852dda1cbaec30b13e6b58b75ab6ff7f7bed
Jan Musinsky <musinsky@gmail.com>
2015-06-30 00:21:07 +0300

    tmp commnit of vmemonitor_client

--- Monitor/client/vmemonitor_client.c
+++ Monitor/client/vmemonitor_client.c
@@ -1,5 +1,5 @@
 // Musinsky Jan
-// 2015-06-15
+// 2015-06-24
 
 #include "vmemonitor_client.h"
 
@@ -41,6 +41,7 @@ int connectServer(const char *host, const char *port)
 
 void closeConnect(int sfd)
 {
+  if (sfd == -1) return;
   if (shutdown(sfd, SHUT_WR) == -1) perror("shutdown failed");
   if (close(sfd) == -1) perror("close socket failed");
 }
@@ -49,19 +50,194 @@ int sendNotify(int sfd, const char *buffer)
 {
   if (buffer == NULL) {
     fprintf(stderr, "no buffer\n");
-    closeConnect(sfd);
     return -1;
   }
 
-  size_t lenbytes = strlen(buffer);
-  ssize_t sentbytes = send(sfd, buffer, lenbytes, 0);
+  size_t lenbytes;   // size_t  {aka long unsigned int}
+  ssize_t sentbytes; // ssize_t {aka long int}
+
+  lenbytes = strlen(buffer);
+  sentbytes = send(sfd, buffer, lenbytes, 0);
   if (sentbytes == -1) perror("send failed");
 
-  closeConnect(sfd);
   if (sentbytes != (ssize_t)lenbytes) {
-    fprintf(stderr, "send not deliver all bytes\n");
+    fprintf(stderr, "sendNotify not deliver all bytes\n");
     return -1;
   }
 
   return 0;
 }
+
+
+
+// TODO: testing
+
+void sendInfo(int sfd, int info, long datafrom, long datato, const char *fname)
+{
+  char buf[NAME_MAX + 1 + sizeof(int) + 2*sizeof(long)]; // 276
+
+  size_t lenbytes;   // size_t  {aka long unsigned int}
+  ssize_t sentbytes; // ssize_t {aka long int}
+
+  lenbytes = 0;
+  memcpy(buf, &info, sizeof(info));                    // info code
+  lenbytes += sizeof(info);
+  memcpy(buf + lenbytes, &datafrom, sizeof(datafrom)); // data from
+  lenbytes += sizeof(datafrom);
+  memcpy(buf + lenbytes, &datato, sizeof(datato));     // data to
+  lenbytes += sizeof(datato);
+  memcpy(buf + lenbytes, fname, strlen(fname));        // data fname
+  lenbytes += strlen(fname);
+
+  sentbytes = send(sfd, buf, lenbytes, 0);
+  if (sentbytes == -1)
+    perror("send failed");
+  if (sentbytes != (ssize_t)lenbytes)
+    fprintf(stderr, "sendInfo not deliver all bytes\n");
+
+
+  //  // Write int value to socket
+  //  void write_int(int sock, int value) {
+  //    const char *data = (const char *)&value;
+  //    write_range(sock, data, data + sizeof(int));
+  //  }
+
+  // dont send number (short, integer) directly (endian problem) only with htonl function
+  // http://stackoverflow.com/questions/13279024/convert-a-uint16-t-to-char2-to-be-sent-over-socket-unix
+  // http://stackoverflow.com/questions/16729496/data-transfer-across-big-and-small-endian-machines
+
+  /* receiving
+  // anything in C can be read or written as an array of char
+  // int a = 1;
+  // char *c = (char*)(&a);
+
+  lenbytes = 0;
+  memcpy(&info,     &buf[lenbytes], sizeof(info));
+  lenbytes += sizeof(info);
+  memcpy(&datafrom, &buf[lenbytes], sizeof(datafrom));
+  lenbytes += sizeof(datafrom);
+  memcpy(&datato,   &buf[lenbytes], sizeof(datato));
+  lenbytes += sizeof(datato);
+  memcpy(fname,     &buf[lenbytes], NAME_MAX + 1);
+  // or
+  memcpy(&fname[0], &buf[lenbytes], NAME_MAX + 1);
+   */
+}
+
+
+
+long sendFile(int sfd, long foffset, FILE *fstream)
+{
+  // send file over socket from foffset to end of the file
+  // return last sended position (next offset) in bytes of the fstream
+
+  if (fstream == NULL) {
+    fprintf(stderr, "no fstream\n");
+    return -1;
+  }
+
+  size_t nread, wordsize, blocksize;
+  ssize_t nsent;
+  long filepos, totread, totsent;
+
+  // na rychlost fread vacsia velkost nema vplyv
+  // preverit aky ma vplyv velkost na rychlost send
+  // share from vmemonitor_file.h (define BLOCK_SIZE) alebo extra
+  blocksize = 4096;
+
+  // TODO citam po int (4bytes) citat jednoducho po (1bytes)
+  // findLastType je potrebny VME aligned, checkujem SHDR alebo EHDR
+  // tu je tato podmienka zbytona (VME aligned)
+  // posielat mozem akykolvek file, nie len delitelny 4
+
+  // share from vmemonitor_file.h (define WORDSIZE) alebo NE
+  wordsize = sizeof(uint32_t); // minimal word size of data is 4 bytes (32 bits)
+  uint32_t buf[blocksize];     // buffer of 4*BLOCK_SIZE = 16384 bytes size
+
+  if (fseek(fstream, foffset, SEEK_SET) != 0) {
+    perror("fseek (sendFile) failed");
+    return -1;
+  }
+
+  filepos = ftell(fstream);
+  if (filepos == -1) {
+    perror("ftell (sendFile) failed");
+    return -1;
+  }
+  if ((filepos % wordsize) != 0) { // VME specifikum
+    fprintf(stderr, "size of file (VME data) not divisible by %lu\n", wordsize);
+    return -1;
+  }
+
+  totread = 0; // in bytes/wordsize
+  totsent = 0; // in bytes
+
+  do {
+    nread = fread(buf, wordsize, blocksize, fstream);
+    if (nread == 0) {
+      printf("nread = 0\n");
+      break;
+    }
+
+    // sendfile() len s regularnym fileom, opatrne ak je to tmpfs
+    // http://stackoverflow.com/questions/13215656/c-sendfile-and-send-difference
+    // na konci zas hovoria ze ten sendfile az take vyhodu nema (s malym chunk)
+    // So, if the image you want to send to a client is in a file, just go for a sendfile.
+    // If you have it in memory (because you processed it somehow, or just generated),
+    // use the approach I mentioned earlier.
+
+    // takze predsa len na file, ktory sa cita zo suboru je asi najlpesie sendfile, neni to general way (only linux)
+    // !!! http://www.linuxcenter.ru/lib/articles/programming/sendfile.phtml !!!
+
+    nsent = send(sfd, buf, nread*wordsize, 0);
+    if (nsent == -1) {
+      perror("send (sendFile) failed");
+      return -1;
+    }
+    if (nsent == 0)
+      fprintf(stderr, "send (transfer) 0 bytes\n");
+
+    totread += nread;
+    totsent += nsent;
+  } while (nread == blocksize);
+
+  totread *= wordsize; // to bytes
+  if (totread != totsent) printf("%ld (totread) != %ld (totsent)\n", totread, totsent);
+
+  if ((foffset + totread) != ftell(fstream)) printf("%ld != %ld\n", (foffset + totread), ftell(fstream));
+
+  // vraj, ze chvilu pockat so zatvorenim, nech server stihne medzi tym zapisat do file
+  // fwrite na server strane zabera urcity cas
+
+  printf("tot read = %ld\n", totread);
+  printf("tot sent = %ld\n", totsent);
+
+  return ftell(fstream);  // foofset + totread
+}
+
+// Nagle not optimal for streams
+
+// https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_MRG/1.2/html/Realtime_Tuning_Guide/sect-Realtime_Tuning_Guide-Application_Tuning_and_Deployment-TCP_NODELAY_and_Small_Buffer_Writes.html
+// As discussed briefly in Transmission Control Protocol (TCP), by default TCP uses Nagle's algorithm to collect small
+// outgoing packets to send all at once. This can have a detrimental effect on latency.
+
+// Using TCP_NODELAY and TCP_CORK to improve network latency
+
+// http://stackoverflow.com/questions/22124098/is-there-any-significant-difference-between-tcp-cork-and-tcp-nodelay-in-this-use
+
+
+// sending streaming data, disabling Nagle's algorithm is a better option. !!!!!!!!!!!!
+// so TCP_NODELAY is used for disabling nagle's algorithm.
+
+// TCP_CORK aggressively accumulates data.
+
+//long tester = 1234567890123456789;
+//char buffer[8];
+//memcpy(buffer, &tester, 8);
+//long tested;
+//memcpy(&tested, buffer, 8);
+
+//root [0] long tester = 123456789012;
+//root [1] char *data = (char *)&tester
+//root [4] long tested
+//root [5] memcpy(&tested, data, 8)
--- Monitor/client/vmemonitor_client.h
+++ Monitor/client/vmemonitor_client.h
@@ -1,5 +1,5 @@
 // Musinsky Jan
-// 2015-06-15
+// 2015-06-24
 
 #ifndef VMEMONITOR_CLIENT_H
 #define VMEMONITOR_CLIENT_H
@@ -9,11 +9,16 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <limits.h>
 
 int connectServer(const char *host, const char *port);
 
 void closeConnect(int sfd);
 
+void sendInfo(int sfd, int info, long datafrom, long datato, const char *fname);
+
 int sendNotify(int sfd, const char *buffer);
+
+long sendFile(int sfd, long foffset, FILE *fstream);
 
 #endif
